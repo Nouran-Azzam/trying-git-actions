44a99c77465ee6e19570b2ce582f79f1bfa39f21
Your Name <you@example.com>
2022-11-11 01:39:33 +0200

    add build & deploy steps

--- nouran.java
+++ nouran.java
@@ -1,5 +1,5 @@
 public class nouran{
        public static void main(String[]args){
-        System.out.println(" MY NAME IS Nouran Azzam ,i love writing in arabic,my last article include this sentences: ");
-       }System.out.println("أكاد أجزم أن بمقدور ابتسامتها أن تنير غرفة عديمه الشموع وبمقدور ضحكتها أن تجلب الدفء لدار عديمة التدفئة ،لم تكن حياتها بسيطه أبدا فى تحدياتها ومع ذلك لم تحمل أى ضغائن ، مؤمنه بدلا من ذلك أن النعم مستمرة وأن الله وليها فى هذة الحياة عليه تتوكل بيقين ثابت وإيمان لا يتزعزع أن رب الخير لا يأتى إلا بالخير  ");
+        System.out.println(" MY NAME IS Nouran Azzam ,i love writing in arabic artical ");
+       }
 }
